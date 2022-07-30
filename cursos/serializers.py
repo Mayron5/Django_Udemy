4e865f7a0d92ed0c9bdb65414409963151a3c261
Mayron Alves <mayronalves@alu.ufc.br>
2022-07-30 13:22:04 -0300

    Inserção de um novo atributo no serializer.

--- cursos/serializers.py
+++ cursos/serializers.py
@@ -1,4 +1,4 @@
-from dataclasses import fields
+from django.db.models import Avg
 from rest_framework import serializers
 from .models import Curso, Avaliacao
 
@@ -27,6 +27,16 @@ class CursoSerializer(serializers.ModelSerializer):
     # Primary Key Related Field
     avaliacoes = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
     
+    media_avaliacoes = serializers.SerializerMethodField()
+
     class Meta:
         model = Curso
         fields = '__all__'
+
+    def get_media_avaliacoes(self, obj):
+        media = obj.avaliacoes.aggregate(Avg('avaliacao')).get('avaliacao__avg')
+        
+        if media is None:
+            return 0
+        
+        return round(media * 2) / 2
